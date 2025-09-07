1c10b9d1dcc38a808876d4486084872dfd292ec1
Henry Wolf <HWolf.binz@gmail.com>
2025-09-07 14:35:14 +0200

    added hero-section elements and started hello world button styling

--- portfolio/src/app/main-content/hero-section/hero-section.component.ts
+++ portfolio/src/app/main-content/hero-section/hero-section.component.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-hero-section',
   imports: [],
   templateUrl: './hero-section.component.html',
-  styleUrl: './hero-section.component.scss'
+  styleUrl: './hero-section.component.scss',
 })
 export class HeroSectionComponent {
+  buttonText = 'Hello world';
 
+  onHover() {
+    this.buttonText = 'I´m Henry Wolf';
+  }
+
+  onLeave() {
+    this.buttonText = 'Hello world';
+  }
 }
