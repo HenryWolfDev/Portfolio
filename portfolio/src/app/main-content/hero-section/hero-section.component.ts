3b5c0e52ef90eda7bc3f81c083642ed5efafabe5
Henry Wolf <HWolf.binz@gmail.com>
2025-09-07 19:38:38 +0200

    moved styles for hello world btn to a new scss styles file

--- portfolio/src/app/main-content/hero-section/hero-section.component.ts
+++ portfolio/src/app/main-content/hero-section/hero-section.component.ts
@@ -1,19 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.scss',
 })
 export class HeroSectionComponent {
-  buttonText = 'Hello world';
-
-  onHover() {
-    this.buttonText = 'I´m Henry Wolf';
-  }
-
-  onLeave() {
-    this.buttonText = 'Hello world';
-  }
+  isHovered = false;
 }
