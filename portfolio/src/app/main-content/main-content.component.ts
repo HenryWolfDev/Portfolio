4918a83690a1f04d6dd91491ae0faeb6d34be03c
Henry Wolf <HWolf.binz@gmail.com>
2025-09-09 22:39:44 +0200

    skill-sect-section-component created

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
+import { SkillSetSectionComponent } from './skill-set-section/skill-set-section.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroSectionComponent, AboutMeSectionComponent],
+  imports: [HeroSectionComponent, AboutMeSectionComponent, SkillSetSectionComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
