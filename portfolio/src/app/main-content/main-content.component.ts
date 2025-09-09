cdc0cbd72e017041a7cb2dcfda5e18334f6ba21c
Henry Wolf <HWolf.binz@gmail.com>
2025-09-09 19:24:14 +0200

    created about-me-section-component

--- portfolio/src/app/main-content/main-content.component.ts
+++ portfolio/src/app/main-content/main-content.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroSectionComponent],
+  imports: [HeroSectionComponent, AboutMeSectionComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss',
 })
