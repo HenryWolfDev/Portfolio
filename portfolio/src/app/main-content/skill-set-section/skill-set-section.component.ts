d5380b292a94fd36305eb3d2d128684fe1d1c3fa
Henry Wolf <HWolf.binz@gmail.com>
2025-09-10 20:17:09 +0200

    created pull-to-peel animation

--- portfolio/src/app/main-content/skill-set-section/skill-set-section.component.ts
+++ portfolio/src/app/main-content/skill-set-section/skill-set-section.component.ts
@@ -4,8 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-skill-set-section',
   imports: [],
   templateUrl: './skill-set-section.component.html',
-  styleUrl: './skill-set-section.component.scss'
+  styleUrl: './skill-set-section.component.scss',
 })
 export class SkillSetSectionComponent {
+  peelOffVisible = false;
+  hideFirtLayer = false;
 
+  togglePeelBox() {
+    this.peelOffVisible = !this.peelOffVisible;
+    this.hideFirtLayer = !this.hideFirtLayer;
+  }
 }
