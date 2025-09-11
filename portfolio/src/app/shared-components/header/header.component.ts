7edbf8a3f8e228f1d324c15b21af3c4929be5518
Henry Wolf <HWolf.binz@gmail.com>
2025-09-11 11:42:05 +0200

    ngx translate added

--- portfolio/src/app/shared-components/header/header.component.ts
+++ portfolio/src/app/shared-components/header/header.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, signal, inject } from '@angular/core';
+import {
+  TranslatePipe,
+  TranslateDirective,
+  TranslateService,
+} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private translate = inject(TranslateService);
 
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
 }
