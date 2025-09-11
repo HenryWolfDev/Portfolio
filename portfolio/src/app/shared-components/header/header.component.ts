f134a96c6e3456198c0c32337c40d5fa9c5e7f96
Henry Wolf <HWolf.binz@gmail.com>
2025-09-11 21:32:20 +0200

    created draw-animation-reverse for header elements organized styles folder

--- portfolio/src/app/shared-components/header/header.component.ts
+++ portfolio/src/app/shared-components/header/header.component.ts
@@ -12,9 +12,31 @@ import {
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  private translate = inject(TranslateService);
+  // private translate = inject(TranslateService);
+  // useLanguage(language: string): void {
+  //   this.translate.use(language);
+  // }
 
-  useLanguage(language: string): void {
-    this.translate.use(language);
+  isHovering = signal(false);
+  hasHovered = signal(false);
+
+  onMouseEnter() {
+    this.hasHovered.set(true);
+    this.isHovering.set(true);
+  }
+
+  onMouseLeave() {
+    this.isHovering.set(false);
+  }
+
+  onAnimationEnd(e: AnimationEvent) {
+    const name = e.animationName;
+    // Nur wenn die Rückwärtsanimation fertig ist => Reset
+    if (
+      name === 'draw-line-reverse' ||
+      (e.target as HTMLElement).classList.contains('draw-reverse')
+    ) {
+      this.hasHovered.set(false); // entfernt draw-reverse via Binding
+    }
   }
 }
