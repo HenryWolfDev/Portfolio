a62f6377bfff5f092276a7c7729f15f4020f5536
Henry Wolf <HWolf.binz@gmail.com>
2025-09-15 22:32:08 +0200

    refactor(contact-section-component) moved to main-content

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -5,7 +5,7 @@ import { SkillSetSectionComponent } from './skill-set-section/skill-set-section.
 import { HeaderComponent } from '../shared-components/header/header.component';
 import { ProjectsSectionComponent } from './projects/projects-section/projects-section.component';
 import { ColleaguesThoughtsSectionComponent } from './colleagues-thoughts-section/colleagues-thoughts-section.component';
-import { ContactSectionComponent } from '../shared-components/contact-section/contact-section.component';
+import { ContactSectionComponent } from './contact-section/contact-section.component';
 
 @Component({
   selector: 'app-main-content',
