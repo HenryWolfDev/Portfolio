d0eea76e53b86ca13920a60f23c6961d9bd07e26
Henry Wolf <HWolf.binz@gmail.com>
2025-09-16 20:03:10 +0200

    feat: refactor display-flex mixin and update styles for consistency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainContentComponent } from './main-content/main-content.component';
-import { FooterComponent } from './shared-components/footer/footer.component';
+
 import { HeaderComponent } from './shared-components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    MainContentComponent,
-    FooterComponent,
-  ],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
