f734dca09399c650f9e4a537a9f1196ea7deb4f6
Henry Wolf <HWolf.binz@gmail.com>
2025-09-18 23:41:53 +0200

    feat: enhance contact form with validation, privacy policy checkbox, and improved styling

--- src/app/main-content/contact-section/contact-section.component.ts
+++ src/app/main-content/contact-section/contact-section.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-section',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-section.component.html',
-  styleUrl: './contact-section.component.scss'
+  styleUrl: './contact-section.component.scss',
 })
 export class ContactSectionComponent {
-
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+    acceptedPolicy: false,
+  };
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+  }
 }
