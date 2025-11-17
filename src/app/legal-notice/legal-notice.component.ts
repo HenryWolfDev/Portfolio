6fb3ff3a131a0ed859af3f52fb48c60fc16cda27
Henry Wolf <HWolf.binz@gmail.com>
2025-11-17 23:29:30 +0100

    feat: add mobile back navigation button to legal notice section and enhance styles for better responsiveness

--- src/app/legal-notice/legal-notice.component.ts
+++ src/app/legal-notice/legal-notice.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../shared-components/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { TranslatePipe } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [FooterComponent],
+  imports: [FooterComponent, CommonModule, TranslatePipe, RouterLink],
   templateUrl: './legal-notice.component.html',
   styleUrl: './legal-notice.component.scss',
 })
